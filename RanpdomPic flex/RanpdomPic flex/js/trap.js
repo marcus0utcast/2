dcdcee297f6edb3a7691a551a2812b39dae578b1
marcus0utcast <115426051+marcus0utcast@users.noreply.github.com>
2022-10-11 18:07:36 +0300

    Add files via upload

---
+++ RanpdomPic flex/RanpdomPic flex/js/trap.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".random");
+const image = document.querySelector(".pic");
+const url = "https://api.waifu.pics/nsfw/trap";
+
+async function fetchHandler() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        image.src = data.url;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+button,addEventListener("click", () => {
+    fetchHandler();
+})
